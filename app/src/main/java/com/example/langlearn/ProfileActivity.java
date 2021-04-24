7d69ce9a07792443ae00beb446fa0d92e88af7d4
Ike Hirzel <hirzelisaac@gmail.com>
2021-04-24 00:52:58 -0700

    Added abstract class to encapsulate common ui elements

--- app/src/main/java/com/example/langlearn/ProfileActivity.java
+++ app/src/main/java/com/example/langlearn/ProfileActivity.java
@@ -1,33 +1,25 @@
 package com.example.langlearn;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.langlearn.ui.login.LangLearnActivity;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends LangLearnActivity {
 
 	private TextView greetingText;
 	private TextView descrText;
-
-	Button btHome;
-	Button btMessage;
-	Button btPost;
-	Button btProfile;
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
-
-		btHome = findViewById(R.id.btHome);
-		btMessage = findViewById(R.id.btMessage);
-		btPost = findViewById(R.id.btPost);
-		btProfile = findViewById(R.id.btProfile);
+//		homeButton = findViewById(R.id.nav_home_button);
+//		messageButton = findViewById(R.id.nav_message_button);
+//		postButton = findViewById(R.id.nav_post_button);
+//		profileButton = findViewById(R.id.nav_profile_button);
+		createButtons();
 
 		greetingText = findViewById(R.id.profile_greeting_text);
 		descrText = findViewById(R.id.profile_descr_text);
@@ -41,44 +33,6 @@ public class ProfileActivity extends AppCompatActivity {
 
 		greetingText.setText("Welcome, " + name + "!");
 		descrText.setText(descr);
-
-		btHome.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(ProfileActivity.this, MainActivity.class);
-				startActivity(i);
-				finish();
-			}
-		});
-
-		btMessage.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(ProfileActivity.this, MessageActivity.class);
-				startActivity(i);
-				finish();
-			}
-		});
-
-		btPost.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(ProfileActivity.this, PostActivity.class);
-				startActivity(i);
-				finish();
-			}
-		});
-
-		btProfile.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(ProfileActivity.this, ProfileActivity.class);
-				startActivity(i);
-				finish();
-			}
-		});
-
-
 	}
 }
 
