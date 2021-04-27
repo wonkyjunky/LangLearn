96caa2d4978ed29cc2d15dac94f65a1e71fd2360
richardLedbetter <cern4life@gmail.com>
2021-04-26 17:02:14 -0700

    redoing the connection to the back4app

--- app/src/main/java/com/example/langlearn/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/langlearn/ui/login/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.example.langlearn.ui.login;
 
+import android.Manifest;
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -24,6 +35,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.langlearn.MainActivity;
 import com.example.langlearn.R;
 import com.example.langlearn.ui.login.LoginViewModel;
 import com.example.langlearn.ui.login.LoginViewModelFactory;
@@ -31,9 +43,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -43,59 +60,139 @@ public class LoginActivity extends AppCompatActivity {
     private LoginViewModel loginViewModel;
     //Google sign in variables
     GoogleSignInClient mGoogleSignInClient;
-    GoogleSignInOptions gso;
-    Map<String, String> logins;
     GoogleSignInAccount account;
+    Map<String, String> logins;
+    ParseUser user;
     //end Google sign in variables
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        user = new ParseUser();
+        // Set the user's username and password, which can be obtained by a forms
+        /*user.setUsername( "<Your username here>");
+        user.setPassword( "<Your password here>");
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    showAlert("Successful Sign Up!", "Welcome" + "<Your username here>" +"!");
+                } else {
+                    ParseUser.logOut();
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });*/
+
+
+         /*google sign in
+        Reference:
+            https://developers.google.com/identity/sign-in/android/start-integrating?authuser=1
+        methods:
+            *signIn
+            *handleSignInResult
+            *signOut
+            *onActivityResult (request code ==1)
+            *
+        */
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("202818144577-a6mms4hlaik35ld031enrkdn8qu87mas.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        final Button loginButton = findViewById(R.id.login);
-        loginButton.setOnClickListener(v1->{
-            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-            startActivityForResult(signInIntent, 1);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(v1->{signIn();});
+
+
+
+        Button sign_out = findViewById(R.id.logout);
+        sign_out.setOnClickListener(v1 -> {
+            mGoogleSignInClient.signOut()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("signedout","done");
+                        }
+                    });
         });
 
-
-
     }
-
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 1) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+    protected void onStart(){
+        super.onStart();
+        account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            Log.d("stuff:",""+account.getIdToken());
         }
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1);
+    }
+
+    private void showAlert(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        // don't forget to change the line below with the names of your Activities
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+    }
+
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Log.d("works",account.getDisplayName());
-            Map<String, String> authData = new HashMap<String, String>();
-            authData.put("access_token", account.getIdToken());
-            authData.put("id", account.getId());
-            ParseUser.logInWithInBackground("google", authData);
+
+
             // Signed in successfully, show authenticated UI.
+            if(account!=null){
+                //authenticating with AWS
+
+                Map<String, String> authData = new HashMap<String, String>();
+                authData.put("access_token", account.getIdToken());
+                authData.put("id", account.getDisplayName());
+                Log.d("please3",account.getDisplayName());
+                Log.d("please2",account.getIdToken());
+                ParseUser.logInWithInBackground("google", authData);
+
+            }else{
+                account = completedTask.getResult(ApiException.class);
+                Map<String, String> authData = new HashMap<String, String>();
+                authData.put("access_token", account.getIdToken());
+                Log.d("please1",account.getIdToken());
+                authData.put("id", account.getDisplayName());
+                ParseUser.logInWithInBackground("google", authData);
+            }
 
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("TAG", "signInResult:failed code=" + e.getMessage());
+            Log.w("TAG", "signInResult:failed code=" );e.printStackTrace();
+            int x = 2+2;
+            //updateUI(null);
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1){//google login activity
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
     }
 
